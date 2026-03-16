e61e61ce7f6b633629ab4ce56683438b9382172c
vladyslavmytko <vladyslavmytko@gmail.com>
2026-03-16 11:13:30 +0000

    Project Initialisation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+const App = () => {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route paht="/about" element={<About />}/>
+    </Routes>
+  )
+
+}
+
+export default App
