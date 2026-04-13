d0eda298c14b6aa2a27839d93e2d8d060e696ad9
vladyslavmytko <vladyslavmytko@gmail.com>
2026-04-13 18:14:17 +0100

    case stydies page is adjusted to mobile/tablet view

--- src/pages/CaseStudies.jsx
+++ src/pages/CaseStudies.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/ReusableComponents/Navbar";
 import { assets_manager } from "../assets/assets_manager";
 import Header from "../components/ReusableComponents/Header";
-import Footer from "../components/ReusableComponents/Footer"
+import Footer from "../components/ReusableComponents/Footer";
 
 const caseStudiesList = [
   {
@@ -91,26 +91,26 @@ const CaseStudies = () => {
         showAboutButton={true}
       />
       <div className="max-w-7xl mx-auto px-6  py-10 container">
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-20 gap-y-10">
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:gap-x-20 gap-x-6 gap-y-10">
           {caseStudiesList.map((item) => (
             <Link
               key={item.id}
               to={`/case-studies/${item.slug}`}
-              className="group bg-transparent flex flex-col w-100 h-75 justify-self-center transform-gpu transition duration-500 ease-out hover:scale-[1.02]"
+              className="group bg-transparent flex flex-col w-full justify-self-center transform-gpu transition duration-500 ease-out hover:scale-[1.02]"
             >
-              <div className="relative w-full h-100 rounded-xl overflow-hidden shadow-xl transition duration-500 ease-out group-hover:shadow-2xl">
+              <div className="relative w-full h-64 sm:min-h-90 rounded-xl overflow-hidden shadow-xl transition duration-500 ease-out group-hover:shadow-2xl">
                 <img
                   src={item.image}
                   alt={item.title}
                   className="w-full h-full object-cover transition duration-700 ease-out group-hover:scale-110"
                 />
 
-                
-
                 {/* content */}
-                <div className="absolute  bottom-0 p-5 text-white flex flex-col gap-3 transition duration-500 ease-out group-hover:-translate-y-2">
-                  <h3 className="text-2xl font-semibold drop-shadow-[0_4px_12px_rgba(0,0,0,0.6)]">{item.title}</h3>
-                  <div className="flex gap-2 text-sm">
+                <div className="absolute bottom-0 p-4 sm:p-5 text-white flex flex-col gap-2 sm:gap-3 transition duration-500 ease-out group-hover:-translate-y-2">
+                  <h3 className="text-xl sm:text-2xl font-semibold drop-shadow-[0_4px_12px_rgba(0,0,0,0.6)]">
+                    {item.title}
+                  </h3>
+                  <div className="flex flex-wrap gap-2 text-xs sm:text-sm">
                     <span className="bg-white/20 px-3 py-1 rounded-full backdrop-blur-sm transition duration-500 ease-out group-hover:bg-white/30">
                       Purchase: {item.purchase}
                     </span>
